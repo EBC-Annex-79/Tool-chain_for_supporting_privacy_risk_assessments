f1fe5f093b5456e5fb08b9f7661254749a132ff2
Emil Tang Kristensen <emiltangkristensen@gmail.com>
2021-11-25 22:40:49 +0100

    Add more datasets

--- Framework/Run/execute_template.py
+++ Framework/Run/execute_template.py
@@ -14,40 +14,41 @@ def run_analyses(json):
     g1 = Graph()
 
     # source namespaces
-    RDF  = NSUtil.get_namespase_rdf()
+    RDF = NSUtil.get_namespase_rdf()
     RDFS = NSUtil.get_namespase_rdfs()
-    OWL  = NSUtil.get_namespase_owl()
-    XSD  = NSUtil.get_namespase_xsd()
+    OWL = NSUtil.get_namespase_owl()
+    XSD = NSUtil.get_namespase_xsd()
     PRIVVULN = NSUtil.get_namespase_base_ontology()
     PRIVVULNV2 = NSUtil.get_namespase_extrantion_ontology()
     SBUILDING = NSUtil.get_namespase_domain_smart_building()
 
-    g1.bind('rdf' , RDF)
+    g1.bind('rdf', RDF)
     g1.bind('rdfs', RDFS)
-    g1.bind('owl' , OWL)
-    g1.bind('xsd' , XSD)
+    g1.bind('owl', OWL)
+    g1.bind('xsd', XSD)
 
     # custom namespace
-    g1.bind('privvuln',PRIVVULN)
+    g1.bind('privvuln', PRIVVULN)
 
-    g1.bind('privvulnv2',PRIVVULNV2)
+    g1.bind('privvulnv2', PRIVVULNV2)
 
-    g1.bind('sbuilding',SBUILDING)
+    g1.bind('sbuilding', SBUILDING)
 
     nodes = json["nodes"]
-    
+
     links = json["links"]
 
     namespace = json["namespace"]
     M = Namespace(namespace)
     g1.bind('m', M)
-    
+
     for node in nodes:
         if "name" not in node and "type" not in node:
             continue
         subject = M[node["name"]]
         g1.add((subject, RDF.type, rdflib.term.URIRef(node["type"])))
-        if "superType" in node and node["superType"] != "" and node["superType"] and node["superType"] != str(PRIVVULNV2.Context):
+        if "superType" in node and node["superType"] != "" and node["superType"] and node["superType"] != str(
+                PRIVVULNV2.Context):
             g1.add((subject, RDF.type, rdflib.term.URIRef(node["superType"])))
 
         if "attributes" in node:
@@ -55,17 +56,22 @@ def run_analyses(json):
                 if "name" not in attribute and "value" not in attribute and "dataType" not in attribute:
                     continue
 
-                if attribute["dataType"] == "int" or attribute["dataType"] == "double" or attribute["dataType"] == "string":
+                if attribute["dataType"] == "int" or attribute["dataType"] == "double" or attribute[
+                    "dataType"] == "string":
                     if attribute["value"] != '' and attribute["value"] != None:
-                        g1.add((subject, rdflib.term.URIRef(str(PRIVVULNV2) + attribute["name"]), Literal(attribute["value"], datatype=rdflib.term.URIRef(str(XSD)+ attribute["dataType"]))))
+                        g1.add((subject, rdflib.term.URIRef(str(PRIVVULNV2) + attribute["name"]),
+                                Literal(attribute["value"],
+                                        datatype=rdflib.term.URIRef(str(XSD) + attribute["dataType"]))))
 
     for link in links:
         if "subject" not in link and "predicate" not in link and "object" not in link:
             continue
         g1.add((rdflib.term.URIRef(link["subject"]), rdflib.term.URIRef(link["predicate"]),
                 rdflib.term.URIRef(link["object"])))
-
-    driver = Driver(debug_mode=True)
+    driver = Driver(debug_mode=True,
+                    domain_path="./Ontologies/wearableprivacyvuln.ttl",
+                    domain_NS=Namespace('https://emikr15.student,sdu.dk/21/10/05/wearableprivacyvunl.ttl#'))
+    # driver = Driver(debug_mode=True)
     print("graph has %s statements." % len(g1))
 
     g1, risk_results, graph = driver.run_with_output(g1)
